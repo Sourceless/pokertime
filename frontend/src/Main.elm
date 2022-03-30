a49fc850a3aa671d64040ae2e2d655ca09beb4b8
Laurence Pakenham-Smith <laurence@sourceless.org>
2022-03-30 17:48:41 +0100

    more screens

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, button, p)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, text, div, button, p, label, input, form)
+import Html.Attributes exposing (class, for, id)
+import Html.Events exposing (onClick, onSubmit)
+import Html.Events.Extra exposing (onChange)
 import String exposing (fromInt)
 import List exposing (map)
 
@@ -11,11 +12,21 @@ type alias VoteOption =
     { name : String
     , value : Maybe Int }
 
-type alias Model =
-    { vote: Maybe VoteOption }
+type Screen
+    = Username
+    | Waiting
+    | Voting
+    | Results
 
-type Msg =
-    Vote VoteOption
+type alias Model =
+    { vote: Maybe VoteOption
+    , username: Maybe String
+    , screen: Screen }
+
+type Msg
+    = Vote VoteOption
+    | SetUsername String
+    | SetScreen Screen
 
 main = Browser.sandbox
        { init = init
@@ -24,13 +35,23 @@ main = Browser.sandbox
        }
 
 init : Model
-init = { vote = Nothing }
+init = { vote = Nothing
+       , username = Nothing
+       , screen = Username }
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
+        SetUsername username ->
+            { model | username = Just username }
         Vote vote ->
             { model | vote = Just vote }
+        SetScreen screen ->
+            case model.username of
+                Just _ ->
+                    { model | screen = screen }
+                Nothing ->
+                    model
 
 numericVoteOption : Int -> VoteOption
 numericVoteOption n =
@@ -66,4 +87,46 @@ voteButton value model =
 view : Model -> Html Msg
 view model =
     div [class "container"]
-        [div [] (map (\vo -> voteButton vo model) voteOptions) ]
+        [(screenToView model.screen) model]
+
+screenToView : Screen -> (Model -> Html Msg)
+screenToView screen =
+    case screen of
+        Username ->
+            usernameView
+        Waiting ->
+            waitingView
+        Voting ->
+            votingView
+        Results ->
+            resultsView
+
+defaultEmpty : Maybe String -> String
+defaultEmpty ms =
+    case ms of
+        Just s ->
+            s
+        Nothing ->
+            ""
+
+usernameView : Model -> Html Msg
+usernameView model =
+    form [onSubmit (SetScreen Waiting)]
+        [ label [ for "username"
+                , class "input-label" ] [ text "What's your name?" ]
+        , input [ id "username"
+                , class "input-text"
+                , onChange (\s -> SetUsername s)
+                ] [ (text << defaultEmpty) model.username ]]
+
+waitingView : Model -> Html Msg
+waitingView model =
+    p [] [button [ onClick (SetScreen Voting)] [text "Simulate vote start"]]
+
+votingView : Model -> Html Msg
+votingView model =
+    div [] (map (\vo -> voteButton vo model) voteOptions)
+
+resultsView : Model -> Html Msg
+resultsView model =
+    p [] [text "results"]
